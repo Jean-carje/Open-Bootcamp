dc07b80fa9ee2b9663f408b07c9a293dc48bded7
Jean Estevez <ctarriba9@gmail.com>
2022-11-19 12:55:22 -0400

    first commit

---
+++ 001-Introducción_programación/002-Variables_Tipos_Datos/TiposDeDatos/Strings/StringsEjercicio/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Practicas de Strings");
+
+        String mystring = "Esto es una cadena";
+
+        System.out.println(mystring);
+
+        System.out.println("concatenar");
+
+        String concatenar = mystring + " de java";
+
+        System.out.println(concatenar);
+
+    }
+}
---
+++ 001-Introducción_programación/002-Variables_Tipos_Datos/TiposDeDatos/TiposDeDatoEjerc/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // variable/identificador
+        long mylong = 34212L;
+        double mydouble = 3.76d;
+        float myfloat = 3.75f;
+        System.out.println("Datos Primitivos practica");
+
+        System.out.println(mylong);
+        System.out.println(mydouble);
+        System.out.println(myfloat);
+
+    }
+}
