16bbc874400324a548c3dc271743b984671dec78
Jean Estevez <ctarriba9@gmail.com>
2022-12-14 20:28:31 -0400

    Tema 04

--- 001-Introducción_programación/001-Ejercicios/Tema4/tema04/src/App.java
+++ 001-Introducción_programación/001-Ejercicios/Tema4/tema04/src/App.java
@@ -1,5 +1,51 @@
 public class App {
-    public static void main(int numeroIf) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {
+        // if ejercicio
+        int numeroIf = 5;
+        if (numeroIf == 0) {
+            System.out.println("numeroIf es 0");
+        } else if (numeroIf > 0) {
+            System.out.println("numeroIf es Positivo");
+        } else {
+            System.out.println("numeroIf es Negativo");
+        }
+
+        // while ejercicio
+        int numeroWhile = -1;
+        while (numeroWhile < 3) {
+            numeroWhile = numeroWhile + 1;
+            System.out.println(numeroWhile);
+        }
+
+        // do-while ejercicio
+        do {
+            System.out.println(numeroWhile);
+        } while (numeroWhile < 3);
+        {
+            numeroWhile = numeroWhile + 1;
+            System.out.println(numeroWhile);
+        }
+
+        // for ejercicio
+        for (int numeroFor = 0; numeroFor <= 3; numeroFor++) {
+            System.out.println(numeroFor);
+        }
+
+        // switch
+        String estacion = "invierno";
+        switch (estacion) {
+            case "primaver":
+                System.out.println("Primavera");
+                break;
+            case "Otoño":
+                System.out.println("Otoño");
+                break;
+            case "Invierno":
+                System.out.println("Invierno");
+                break;
+            default:
+                System.out.println("Verano");
+                break;
+        }
     }
 }
